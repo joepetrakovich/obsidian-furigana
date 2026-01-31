a306300c8340514067f9eb73ed5b411adc084ce3
Joe Petrakovich <jpetrakovich@gmail.com>
2026-01-31 09:53:44 +0900

    fixed last of lints

--- src/settings.ts
+++ src/settings.ts
@@ -29,63 +29,69 @@ export class SettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new SettingGroup(containerEl)
-			.addSetting(setting => setting
-				.setName('Only show on hover')
-				.addToggle(toggle => toggle
-					.setValue(this.plugin.settings.showOnHover)
-					.onChange(async (value) => {
-						this.plugin.settings.showOnHover = value;
-						await this.plugin.saveSettings();
-						this.plugin.loadStyles();
-						this.display();
+			.addSetting(setting => {
+				setting
+					.setName('Only show on hover')
+					.addToggle(toggle => {
+						toggle
+							.setValue(this.plugin.settings.showOnHover)
+							.onChange(async (value) => {
+								this.plugin.settings.showOnHover = value;
+								await this.plugin.saveSettings();
+								this.plugin.loadStyles();
+								this.display();
+							});
+					});
+			})
+			.addSetting(setting => {
+				setting
+					.setName('Font size')
+					.addSlider(slider => {
+						slider
+							.setDynamicTooltip()
+							.setValue(this.plugin.settings.fontSize)
+							.setLimits(1, 36, 1)
+							.onChange(async (value) => {
+								this.plugin.settings.fontSize = value;
+								await this.plugin.saveSettings();
+								this.plugin.loadStyles();
+								this.display();
+							});
+					});
+			})
+			.addSetting(setting => {
+				setting
+					.setName('Font color')
+					.addExtraButton(button => {
+						button
+							.setDisabled(this.plugin.settings.fontColor === DEFAULT_SETTINGS.fontColor)
+							.setIcon('rotate-ccw')
+							.setTooltip('Restore default')
+							.onClick(async () => {
+								this.plugin.settings.fontColor = DEFAULT_SETTINGS.fontColor;
+								await this.plugin.saveSettings();
+								this.plugin.loadStyles();
+								this.display();
+							});
+						button.extraSettingsEl.setAttribute('aria-disabled', button.disabled ? 'true' : 'false');
 					})
-				))
-			.addSetting(setting => setting
-				.setName('Font size')
-				.addSlider(slider => slider
-					.setDynamicTooltip()
-					.setValue(this.plugin.settings.fontSize)
-					.setLimits(1, 36, 1)
-					.onChange(async (value) => {
-						this.plugin.settings.fontSize = value;
-						await this.plugin.saveSettings();
-						this.plugin.loadStyles();
-						this.display();
-					})
-				))
-			.addSetting(setting => setting
-				.setName('Font color')
-				.addExtraButton(button => button
-					.setDisabled(this.plugin.settings.fontColor === DEFAULT_SETTINGS.fontColor)
-					.setIcon('rotate-ccw')
-					.setTooltip('Restore default')
-					.onClick(async () => {
-						this.plugin.settings.fontColor = DEFAULT_SETTINGS.fontColor;
-						await this.plugin.saveSettings();
-						this.plugin.loadStyles();
-						this.display();
-
-					})
-					.extraSettingsEl.setAttribute('aria-disabled', button.disabled ? 'true' : 'false')
-				)
-				.addColorPicker(color => {
-					let currentValue = this.plugin.settings.fontColor;
-					if (currentValue === DEFAULT_SETTINGS.fontColor) {
-						currentValue = getComputedStyle(document.body)
-							.getPropertyValue('--text-normal')
-							.trim();
-					}
-					color
-						.setValue(currentValue)
-						.onChange(async (value) => {
-							this.plugin.settings.fontColor = value;
-							await this.plugin.saveSettings();
-							this.plugin.loadStyles();
-							this.display();
-						})
-				}))
-
-		// new SettingGroup(containerEl)
+					.addColorPicker(color => {
+						let currentValue = this.plugin.settings.fontColor;
+						if (currentValue === DEFAULT_SETTINGS.fontColor) {
+							currentValue = getComputedStyle(document.body)
+								.getPropertyValue('--text-normal')
+								.trim();
+						}
+						color
+							.setValue(currentValue)
+							.onChange(async (value) => {
+								this.plugin.settings.fontColor = value;
+								await this.plugin.saveSettings();
+								this.plugin.loadStyles();
+								this.display();
+							});
+					});
+			})
 			.addSetting(setting => {
 				setting.setName("Preview")
 				if (this.plugin.settings.showOnHover) {
@@ -105,31 +111,33 @@ export class SettingTab extends PluginSettingTab {
 
 		new SettingGroup(containerEl)
 			.setHeading("Dictionary")
-			.addSetting(async (setting) => {
-				const isDownloaded = await this.plugin.dictionaryManager.isDownloaded();
+			.addSetting((setting) => {
+				void (async () => {
+					const isDownloaded = await this.plugin.dictionaryManager.isDownloaded();
 
-				if (isDownloaded) {
-					setting
-						.setName("Dictionary is installed.")
-						.setDesc(`Dictionary files are stored in "vault/${this.plugin.dictionaryManager.getDataPath()}".`);
-				} else {
-					const warning = document.createDocumentFragment();
-					warning.createSpan({ text: "A 20.5 MB dictionary file is required to use this plugin.", cls: "mod-warning" });
+					if (isDownloaded) {
+						setting
+							.setName("Dictionary is installed.")
+							.setDesc(`Dictionary files are stored in "vault/${this.plugin.dictionaryManager.getDataPath()}".`);
+					} else {
+						const warning = document.createDocumentFragment();
+						warning.createSpan({ text: "A 20.5 MB dictionary file is required to use this plugin.", cls: "mod-warning" });
 
-					setting
-						.setName("Download dictionary")
-						.setDesc(warning)
-						.addButton(async (button) => {
-							button
-								.setButtonText("Download").setCta()
-							.onClick(async () => {
-								await this.plugin.dictionaryManager.downloadDictionary();
-								this.plugin.settings.dataDownloadVersionTag = DEFAULT_SETTINGS.dataDownloadVersionTag;
-								await this.plugin.saveSettings();
-								this.display()
-							})
-						});
-				}
+						setting
+							.setName("Download dictionary")
+							.setDesc(warning)
+							.addButton(button => {
+								button
+									.setButtonText("Download").setCta()
+									.onClick(async () => {
+										await this.plugin.dictionaryManager.downloadDictionary();
+										this.plugin.settings.dataDownloadVersionTag = DEFAULT_SETTINGS.dataDownloadVersionTag;
+										await this.plugin.saveSettings();
+										this.display();
+									});
+							});
+					}
+				})();
 			});
 	}
 }
